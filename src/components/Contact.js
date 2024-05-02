573c42631432f93ce9df3544b5be9a6c3c84139c
Spykoz <carenjot.aurelien@hotmail.fr>
2024-05-02 09:25:09 +0200

    margin responsive for contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,7 +15,7 @@ const Contact = () => {
         initial="hidden"
         whileInView={"show"}
         viewport={{once: false, amount:0.3}}
-        className='flex-1 flex justify-start ml-20 mb-20'>
+        className='flex-1 flex justify-start ml-20 mb-40'>
           <div>
             <h4 className='text-x1 uppercase text-sky-400 font-medium mb-2 tracking-wide'>Entrez en contact</h4>
             <h2 className='text-[45px] lg:text-[90px] leading-none mb-12'>Travaillons <br/>
@@ -28,7 +28,7 @@ const Contact = () => {
         initial="hidden"
         whileInView={"show"}
         viewport={{once: false, amount:0.3}}
-        className='flex-1 border rounded-2xl flex flex-col gap-y-8 p-6 items-center'>
+        className='flex-1 border rounded-2xl flex flex-col gap-y-8 p-6 mb-20 items-center'>
           <div><h2 className='text-x1 uppercase text-sky-400 font-medium mb-2 tracking-wide inline'>Numéro : </h2><h3 className='inline'> 06.49.61.34.07</h3></div>
           <div><h2 className='text-x1 uppercase text-sky-400 font-medium mb-2 tracking-wide inline'>Mail : </h2><h3 className='inline'>carenjot.aurelien@hotmail.fr</h3></div>
           <div className='flex flex-col items-center'><h2 className='text-x1 uppercase text-sky-400 font-medium mb-2 tracking-wide'>Mon CV : </h2><button className=' btn btn-sm lg:btn-lg '><a href={CV} download>Télécharger mon CV</a></button></div>
